c4a32434c3dd65f4e48fc24e8cd4cd627d490f4b
Kiowa Scott <97994294+kscott675@users.noreply.github.com>
2024-03-19 19:42:18 +0000

    authorize @user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ show liked feed followers following discover ]
 
+  def show
+    authorize @user
+  end
   private
 
     def set_user
--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -7,6 +7,8 @@ class UserPolicy
   end
 
   def show?
-    user == current_user || !user.private? || user.followers.include?(current_user)
+    user == current_user ||
+     !user.private? || 
+     user.followers.include?(current_user)
   end
 end
